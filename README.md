55d108a263441cc17dbec2ce3c8419b4b4ea111d
SiriusKoan <k.peihsun@gmail.com>
2021-08-05 13:46:08 +0800

    Add error page

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Shorten URL with KV
 ![Flask](https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white)  
 A URL shortener made with Cloudflare KV and Flask.
+
+## deploy
+You should set
+```
+FLASK_ENV="PRODUCTION"
+FLASK_APP="manage.py"
+EMAIL=<YOUR CLOUDFLARE EMAIL>
+KV_ACCOUNT_IDENTIFIER=<YOUR KV ACCOUNT IDENTIFIER>
+KV_NAMESPACE_IDENTIFIER=<YOUR KV NAMESPACE IDENTIFIER>
+X_AUTH_KEY=<YOUR CLOUDFLARE X-AUTH-KEY>
+```
+and then run `flask run` or use gunicorn
--- app/main/error_handlers.py
+++ app/main/error_handlers.py
@@ -1,17 +1,9 @@
-from flask import redirect, url_for
+from flask import render_template
+from werkzeug.exceptions import HTTPException
 from . import main_bp
 
 
-@main_bp.app_errorhandler(404)
-def not_found_handler(e):
-    return "", 404
-
-
-@main_bp.app_errorhandler(401)
-def unauthorized_handler(e):
-    return redirect(url_for("user.login_page"))
-
-
-@main_bp.app_errorhandler(403)
-def forbidden_handler(e):
-    return "", 403
+@main_bp.app_errorhandler(HTTPException)
+def handler(e):
+    print(e)
+    return render_template("error.html", code=e.code, name=e.name), e.code
